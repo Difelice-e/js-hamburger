56fb31d5fee471fc26391537355f845d1877edb8
Difelice-e <difelice.emanuele@gmail.com>
2022-02-08 16:25:18 +0100

    richiamo variabili

---
+++ main.js
@@ -0,0 +1,9 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+console.log(hamburgerMenu);
+
+const hamburgerIcon = document.querySelector('.fas.fa-bars');
+console.log(hamburgerIcon);
+
+const hamburgerClose = document.querySelector('.close')
+console.log(hamburgerClose)
+
