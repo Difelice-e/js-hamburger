3f85939abd8da5995838c306c849aed7a03832e9
Difelice-e <difelice.emanuele@gmail.com>
2022-02-08 16:31:38 +0100

    soluzione esercizio

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-console.log(hamburgerMenu);
+// richiamo elemento menù
+const hamburgerMenu = document.querySelector('.hamburger-menu'); 
+// console.log(hamburgerMenu);
 
+// richiamo elemento "icona hamburger" 
 const hamburgerIcon = document.querySelector('.fas.fa-bars');
-console.log(hamburgerIcon);
+// console.log(hamburgerIcon);
 
+// richiamo elemento "icona chiusura"
 const hamburgerClose = document.querySelector('.close')
-console.log(hamburgerClose)
+// console.log(hamburgerClose);
+
+hamburgerIcon.addEventListener ("click",   // selezione evento "click" su icona hamburger
+    function() {
+        hamburgerMenu.style.display="block";   // la proprietà display block sul menù lo rende visibile
+    }
+);
+
+hamburgerClose.addEventListener ("click",   // selezione evento "click" su icona di chiusura
+    function() {
+        hamburgerMenu.style.display="none";   // la proprità display none sul menù lo fa scomparire
+    }
+);
+
+
+
+
+
 
